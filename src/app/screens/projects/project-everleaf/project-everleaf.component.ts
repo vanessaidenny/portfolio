a63817656324935032c0c7fa05b43f9f10fa68e1
Vanessa Isabela Denny <vanessaidenny@hotmail.com>
2024-05-13 12:53:16 -0400

    Added project link

--- src/app/screens/projects/project-everleaf/project-everleaf.component.ts
+++ src/app/screens/projects/project-everleaf/project-everleaf.component.ts
@@ -7,10 +7,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./project-everleaf.component.css']
 })
 export class ProjectEverleafComponent {
+  prototypeLink = 'https://www.figma.com/proto/NrqKrdMZ5Ok9WjPP5M2Vwd/UX-Design-II---Project-Showcase---Group-3---Hi-Fi-Wireframing-and-Prototyping?page-id=0%3A1&type=design&node-id=583-6568&viewport=887%2C28%2C0.03&t=DKsL85YEUPYKo49w-1&scaling=scale-down&starting-point-node-id=583%3A6568&mode=design'
 
   constructor(private router: Router) {}
 
   changePage(page: string) {
     this.router.navigate([page]);
+  }
+
+  goToUrl() {
+    window.open(this.prototypeLink, '_blank');
   }
 }
