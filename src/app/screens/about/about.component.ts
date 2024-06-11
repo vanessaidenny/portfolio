3bf9974e49b52c6e51cfc3c6f2ca8389e2ba3e77
Vanessa Isabela Denny <vanessaidenny@hotmail.com>
2024-06-11 15:13:59 -0400

    Replace text and resume link

--- src/app/screens/about/about.component.ts
+++ src/app/screens/about/about.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AboutComponent {
-  resumeUrl = 'https://1drv.ms/b/s!AiyDjPpdmGKKidcm-vexUgb2rMRN5g?e=x6jbgs';
+  resumeUrl = 'https://1drv.ms/b/s!AiyDjPpdmGKKidc6nsAjyQdRrMZiNw?e=bx4ZOj';
 
   constructor(private router: Router) {}
 
